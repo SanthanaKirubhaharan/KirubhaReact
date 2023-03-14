804bfcb05e1f7daa75de25a490a34246d93702dc
Munib Butt <munibrbutt@rogers.com>
2023-03-14 13:21:20 -0400

    Added initial code

---
+++ Default Implementation of Interfaces/DefaultImplementationsInterfaces/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DefaultImplementationsInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEmployee employee = new Employee(2500);
+            Console.WriteLine($"The Tax amount is {employee.GetTaxAmount()}");
+            Console.ReadKey();
+        }
+    }
+
+    public interface IEmployee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public double GetTaxAmount()
+        {
+            return Salary * 0.05;
+        }
+    }
+
+    public class Employee : IEmployee
+    {
+        public Employee(double salary)
+        {
+            Salary = salary;
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+
+
+}
